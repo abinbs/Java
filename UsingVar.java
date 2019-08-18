05b3df0d554e796caae7039ea7e396ce564ec6f5
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-18 20:38:18 +0530

    using variables in Java

---
+++ UsingVar.java
@@ -0,0 +1,14 @@
+public class UsingVar {
+
+    public static void main(String[] args){
+
+        int no = 3;
+        double pi = 3.14;
+        char ch='a';
+
+        System.out.println(no); //prints the  integer value in variable no ie,3
+        System.out.println(pi); //prints the double value in variable no ie,3.14
+        System.out.println(ch); //prints the character value in variable no ie,a
+
+    }
+}
