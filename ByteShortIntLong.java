e5334263b1bed4c43d9737a130c6c6a34396cc63
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-18 20:35:41 +0530

    details on byte,short,int,long data types

---
+++ ByteShortIntLong.java
@@ -0,0 +1,35 @@
+public class ByteShortIntLong {
+
+    //This file contains more information on numbers
+
+
+    //byte has width of 8
+    byte BMin = -128; //minimum value of byte type
+    byte BMax  = 127; //maximum value of byte type
+
+
+    //byte has width of 16
+    short SMin = -32768; //minimum value of short type
+    short SMax  = 32767; //maximum value of short type
+
+
+    //byte has width of 32
+    int IMin = -2147483648; //minimum value of int type
+    int IMax  = 2147483647; //maximum value of int type
+
+
+    //byte has width of 64
+    long LMin = -9_223_372_036_854_775_808L; //minimum value of long type
+    long LMax  = 9_223_372_036_854_775_807L; //maximum value of long type
+
+/*
+* Byte - byte is the smallest data type for storing integers
+*
+* Short - short can store bigger values than byte but less then int data type
+*
+*  Int - int data type is most common for storing in storing integers
+*
+* Long - We use long for extremely large numbers
+* */
+
+}
