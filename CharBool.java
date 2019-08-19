8ee4f5a1293397b89dbda2b6ef9566887ed03f89
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-19 23:15:00 +0530

    information about float and double data type

--- CharBool.java
+++ CharBool.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class CharBool {
+
+    public static void main(String[] args){
+
+        char ucharac = '\u0126';
+
+        char charac = 'W';
+
+        boolean isprime = true;
+
+        System.out.println("Unicode Character::" + ucharac);
+
+
+        System.out.println("Normal Character::" + charac);
+
+        System.out.println("Boolean::" +  isprime);
+
+    }
 }
