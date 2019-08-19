b064b2bb507a51549025ea6ce6447e089e7af72b
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-19 23:14:46 +0530

    information about float and double data type

--- FloatDouble.java
+++ FloatDouble.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class FloatDouble {
+
+    public static void main(String[] args){
+
+        //This file contains more information on decimal numbers
+
+        float fpi =(float) 22f/7f ; //float in brackets is used for typecasting the result to float
+
+        double dpi = (double) 22d/7d ; //double in brackets is used for typecasting the result to double
+
+        System.out.println("PI value in float type = " + fpi); //printing the value of pi
+
+        System.out.println("PI value in double type = " + dpi);//printing the value of pi
+
+        /*
+        * float -> a data type to store decimal numbers,7 places after the decimal point(takes 32 bits of space)
+        *
+        * double -> another data type to store decimal numbers,16 places after the decimal point(takes 64 bits of space)
+        *
+        * Note:::type-casting is used to convert the data type with ease
+        * */
+    }
 }
