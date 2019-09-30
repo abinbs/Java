cd244c64e9354cdb609f5002f47b0326ae435f62
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-30 20:06:48 +0530

    Update UsingVar.java

--- UsingVar.java
+++ UsingVar.java
@@ -5,10 +5,13 @@ public class UsingVar {
         int no = 3;
         double pi = 3.14;
         char ch='a';
+        float c=5.01;
+        boolean value=false;
 
         System.out.println(no); //prints the  integer value in variable no ie,3
         System.out.println(pi); //prints the double value in variable no ie,3.14
         System.out.println(ch); //prints the character value in variable no ie,a
-
+        System.out.println(c);//prints the floating point valiue in variable ie,5.01
+        System.out.println(value);//prints the boolean variable ie, false
     }
 }
