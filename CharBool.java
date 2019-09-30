5ffac5b698437f078413f36d5fb8ce44264b6b0d
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-30 20:20:50 +0530

    Update CharBool.java

--- CharBool.java
+++ CharBool.java
@@ -14,6 +14,14 @@ public class CharBool {
         System.out.println("Normal Character::" + charac);
 
         System.out.println("Boolean::" +  isprime);
+        
+        /* The Boolean data type is used to store only two possible values: true and false.
+        This data type is used for simple flags that track true/false conditions.
+        
+        The char data type is a single 16-bit Unicode character.
+        Its value-range lies between '\u0000' (or 0) to '\uffff' (or 65,535 inclusive).
+        The char data type is used to store characters
+        */
 
     }
 }
